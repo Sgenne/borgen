6c1d31fc9a551722640022b575fee61daab5199d
GENNE Simon <simon.genne@soprasteria.com>
2025-12-31 16:39:47 +0100

    Renamed css file

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import AppRouter from "./AppRouter";
-import "./app.css";
+import "./main.css";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
